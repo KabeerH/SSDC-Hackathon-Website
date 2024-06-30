4be8a0c5df492f27ba7be0efe31eb683d3ac68d4
KabeerH <kabeerharjani@gmail.com>
2024-06-30 02:18:45 -0400

    improved Hamburger Nav-Bar

--- components/MainNav.js
+++ components/MainNav.js
@@ -41,7 +41,7 @@ export default function Navbar() {
             </div>
           </div>
           <div className="-mr-2 flex md:hidden">
-            <button onClick={() => setIsOpen(!isOpen)} className="inline-flex items-center justify-center p-2 rounded-md text-red-8 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white transition-colors duration-200">
+            <button onClick={() => setIsOpen(!isOpen)} className="inline-flex items-center justify-center p-2 rounded-md text-red-600 hover:text-white">
               {!isOpen ? (
                 <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
